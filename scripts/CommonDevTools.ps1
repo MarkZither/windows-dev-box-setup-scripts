a311e56c0171f896b99f94c726a0ee273c4d12e8
MarkZither <mark.burton@zither-it.co.uk>
2022-04-12 21:42:38 +0200

    Update common dev tools script with new software

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -2,7 +2,7 @@
 # tools we expect devs across many scenarios will want
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python
+choco install -y python3
 choco install -y 7zip.install
 choco install -y sysinternals
 choco install -y smtp4dev
@@ -12,11 +12,11 @@ choco install -y keepass
 #choco install -y windirstat
 choco install -y wiztree
 choco install -y tortoisesvn
-choco install -y mattermost-desktop
+choco install -y tortoisegit
 choco install -y conemu
 choco install -y greenshot
 choco install -y notepadplusplus.install
 choco install -y sumatrapdf
 choco install -y vlc
 choco install -y dotpeek
-choco install microsoft-windows-terminal
+choco install -y microsoft-windows-terminal
