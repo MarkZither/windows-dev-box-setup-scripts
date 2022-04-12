7d9d5c91280f2c200f3bcd94bcb556fbe0050ebe
MarkZither <mark.burton@zither-it.co.uk>
2022-04-12 21:48:51 +0200

    Update WSL Ubuntu to 2004

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -2,6 +2,7 @@ choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
 
 #--- Ubuntu ---
 # TODO: Move this to choco install once --root is included in that package
+<#
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
 Add-AppxPackage -Path ~/Ubuntu.appx
 # run the distro once and have it install locally with root user, unset password
@@ -10,6 +11,18 @@ RefreshEnv
 Ubuntu1804 install --root
 Ubuntu1804 run apt update
 Ubuntu1804 run apt upgrade -y
+#>
+<#
+choco install -y wsl-ubuntu-2004
+#>
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile ~/Ubuntu2004.appx -UseBasicParsing
+Add-AppxPackage -Path ~/Ubuntu2004.appx
+# run the distro once and have it install locally with root user, unset password
+
+RefreshEnv
+Ubuntu2004 install --root
+Ubuntu2004 run apt update
+Ubuntu2004 run apt upgrade -y
 
 <#
 NOTE: Other distros can be scripted the same way for example:
