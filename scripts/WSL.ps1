da5c177d565a82f7ee30497770d368aca6f823b4
MarkZither <mark.burton@zither-it.co.uk>
2022-04-12 21:53:42 +0200

    Update WSL install to WSL2

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -1,4 +1,4 @@
-choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
+choco install -y choco install wsl2 --params "/Version:2 /Retry:true"
 
 #--- Ubuntu ---
 # TODO: Move this to choco install once --root is included in that package
