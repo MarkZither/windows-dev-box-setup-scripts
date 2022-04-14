f9e70636f8af1fdd87114b6dc5df72bbe6b5f7d9
MarkZither <mark.burton@zither-it.co.uk>
2022-04-14 22:35:54 +0200

    set git install to use SChannel

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,7 @@
 
 # tools we expect devs across many scenarios will want
 choco install -y vscode
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal /SChannel /Editor:VisualStudioCode'"
 choco install -y python3
 choco install -y 7zip.install
 choco install -y sysinternals
