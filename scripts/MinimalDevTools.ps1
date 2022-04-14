d2e7c0abead1dd635946ffe3bcd320e82d71050d
MarkZither <mark.burton@zither-it.co.uk>
2022-04-14 22:02:08 +0200

    Add minimal devtools script

---
+++ scripts/MinimalDevTools.ps1
@@ -0,0 +1,11 @@
+
+# tools we expect devs across many scenarios will want
+choco install -y vscode
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal /SChannel /Editor:VisualStudioCode'"
+choco install -y 7zip.install
+choco install -y winmerge
+choco install -y keepass
+choco install -y tortoisesvn
+choco install -y tortoisegit
+choco install -y greenshot
+choco install -y microsoft-windows-terminal
