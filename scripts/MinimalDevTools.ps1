ccfde8da21afbb23819bf1c1572dc56d97921a92
MarkZither <mark.burton@zither-it.co.uk>
2022-04-14 22:36:21 +0200

    Add svn command line to minimal tools

--- scripts/MinimalDevTools.ps1
+++ scripts/MinimalDevTools.ps1
@@ -2,10 +2,11 @@
 # tools we expect devs across many scenarios will want
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal /SChannel /Editor:VisualStudioCode'"
+choco install -y tortoisegit
 choco install -y 7zip.install
 choco install -y winmerge
 choco install -y keepass
+choco install -y svn
 choco install -y tortoisesvn
-choco install -y tortoisegit
 choco install -y greenshot
 choco install -y microsoft-windows-terminal
